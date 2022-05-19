ec2295e7e33566f32c8c50d47d08cb2153187395
Grecia1933 <97680142+Grecia1933@users.noreply.github.com>
2022-05-19 07:37:59 -0600

    Create Matriz.java

---
+++ Matriz.java
@@ -0,0 +1,8 @@
+// Matriz de Adyacencia 
+typedef struct_graph 
+{
+        char vexs [MAX]; // Colección de vértices 
+        int vexnum; // Número de vértices 
+        int edgnum; // Número de aristas 
+        matriz int [MAX] [MAX]; // Matriz de adyacencia 
+        }
