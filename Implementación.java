59b6674deb749a9e7b714828b267f9ac6fde409a
Grecia1933 <97680142+Grecia1933@users.noreply.github.com>
2022-05-19 07:47:53 -0600

    Create Implementación.java

---
+++ Implementación.java
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std; 
+
+class Graph_DG { 
+  private:
+    int vexnum; // Número de vertices del gráfico 
+    int edge; // Número de aristas del gráfico 
+    int arc; // Matriz de Adyacencia 
+    int dis; // Registro de la información acerca de la ruta más corta entre cada vertice 
+    int path; // Registro de la información de cada ruta corta 
+  
+public : 
+  // Constructor 
+  Graph_DG(int vexnum, int edge); 
+  // Destructor 
+   ~Graph_DG();
+    // Verificar si la información del borde ingresada es legal cada vez
+    // Los vértices están numerados de 1
+    bool check_edge_value(int start, int end, int weight);
+    // Crear gráfico
+    void createGraph(int);
+    // Imprimir la matriz de adyacencia
+    void print();
+    // Encuentrar el camino más corto
+    void Floyd();
+    // Imprimir el camino más corto
+    void print_path();
+};
